778742960bb1264ffeceba34805be2b486e08047
JimClifford <92957332+JimClifford@users.noreply.github.com>
2023-11-16 11:58:21 +0000

    Update churn.py
    Commented

--- churn.py
+++ churn.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import pandas as pd
 import pickle
-# import tensorflow
-# import scikeras
+
 
 model_path = 'best_model.pkl'
 scaler_path = 'scaler.pkl'
-
+#model
 loaded_model = pickle.load(open(model_path, 'rb'))
+#scaler object
 scaler = pickle.load(open(scaler_path, 'rb'))
+# accuracy score of model
 accuracy_of_model = 82.55
+
 # Function to preprocess user input
 def preprocess_input(user_data, scaler):
-    # Assuming features are stored in a DataFrame named input_data
+    # creating a dataframe from user input
     input_data = pd.DataFrame(user_data, index=[0])
 
     # Use the loaded scaler to transform the input data
@@ -22,22 +24,23 @@ def preprocess_input(user_data, scaler):
 
 # Function to predict player rating
 def predict_churn(scaled_input_data, model):
-    # Assuming the model takes the scaled input data
+    
     user_churn = model.predict(scaled_input_data)
     return user_churn
 
 # Create a Streamlit web app
 st.title("Customer Churn Predictor")
 
-# Create input fields for player data
-st.write("Enter Player Data:")
+# Create fields for user inputs
+st.write("Enter User features:")
 user_data = {}  # Create a dictionary to store user input
 
-# Define the feature names in Xtest
+# Define the feature names in Xtest - 10 
 features = ['TotalCharges','MonthlyCharges','tenure','Contract','PaymentMethod','TechSupport','OnlineSecurity','gender','InternetService','OnlineBackup']
 
 # Loop through the features and create input fields
 for feature in features:
+    # Note that the min/max values are informed by the dataset characterisitcs.
     if (feature == 'gender'):
         user_data[feature] = st.number_input(f"{feature.capitalize()}: Enter 0(Female) or 1(Male)", min_value=0, max_value=1)
      
@@ -77,12 +80,10 @@ for feature in features:
 
     
 
-
+# process user input
 input_data = preprocess_input(user_data,scaler)
 if st.button("Predict"):
-    # Convert the input data to a DataFrame for prediction
-    #input_data = pd.DataFrame(player_data, index=[0])
-
+    
     # Call the prediction function
     user_churn = predict_churn(input_data,loaded_model)
     user_churn = user_churn.astype(int)
@@ -92,6 +93,6 @@ if st.button("Predict"):
     else:
         prediction = "No"
     
-    # Display the predicted player rating
+    # Display the prediction of churn and accuracy score as a measure of confidence
     st.write(f"Will User Churn: {prediction}")
     st.write(f"Model Accuracy: {accuracy_of_model}")
